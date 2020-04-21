649c12114adbc157698c0ef6aed76f24d54e4705
Chukwudi Ugorji <cp.ugorji@gmail.com>
2020-04-22 00:21:27 +0100

    refactoring fault tolerance capability on the gateway service

--- APICloudGateway/src/main/java/com/technologies/stealth/cloud/gateway/controller/FallbackController.java
+++ APICloudGateway/src/main/java/com/technologies/stealth/cloud/gateway/controller/FallbackController.java
@@ -5,7 +5,6 @@
  */
 package com.technologies.stealth.cloud.gateway.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -15,14 +14,13 @@ import reactor.core.publisher.Mono;
  * @author chukwudiugorji
  */
 @RestController
-@RequestMapping("/api/v1/failure")
 public class FallbackController {
-    @GetMapping("/orderFallback")
+    @RequestMapping("/orderFallback")
     public Mono<String> orderServiceFallback(){
         return Mono.just("Order service is taking too long to respond or probably down, please try again later");
     }
     
-    @GetMapping("/paymentFallback")
+    @RequestMapping("/paymentFallback")
     public Mono<String> paymentServiceFallback(){
         return Mono.just("Payment service is taking too long to respond or probably down, please try again later");
     }
